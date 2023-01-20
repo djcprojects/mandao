d6333c59195f257c675ca904ea93ca0c9c66ca1f
djcprojects <djruncode@gmail.com>
2023-01-20 12:32:30 -0400

    Form commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,8 @@
 import "../styles/Hero.css"
+import { Link } from "react-router-dom"
+import user from "../img/user.png"
+import email from "../img/email.png"
+import key from "../img/key.png"
 
 
 export default function Hero() {
@@ -13,7 +17,26 @@ export default function Hero() {
             Text goes here
         </div>
         <div className="form">
-            Form goes here
+            <form>
+                <h2>Register Now</h2>
+                <div className="input-content">
+                    <img src={user} />
+                    <input type="text" placeholder="Username" />
+                </div>
+                <div className="input-content">
+                    <img src={email} />
+                    <input type="email" placeholder="Email" />
+                </div>
+                <div className="input-content">
+                    <img src={key} />
+                    <input type="password" placeholder="Password" />
+                </div>
+                <div className="check-box">
+                    <label id="pass"><input type="checkbox" id="pass" /> Remember your password</label>
+                    <label id="terms"><input type="checkbox" id="terms" /> I accept the <Link>terms and conditions of use</Link> </label>
+                    <button type="submit">Sign up</button>
+                </div>
+            </form>
         </div>
     </div>
   )
